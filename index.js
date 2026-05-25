1352f0841e34b8ce2f442efa028aabce921097bf
yaso2134 <yaso84206@gmail.com>
2026-05-25 22:18:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('Bot is Alive!'));
-app.listen(3000, () => console.log('Server is running on port 3000'));
-
-const client = new Client({ 
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] 
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`البوت شغال: ${client.user.tag}`);
 });
 
 client.on('messageCreate', (message) => {
-    if (message.author.bot) return;
-    if (message.content === '!شغل') message.reply('جاري العمل...');
-    if (message.content === '!بنك') message.reply('رصيدك هو 0 ｍ حالياً.');
+  if (message.content === '!تجربة') {
+    message.reply('البوت شغال 100% يا وحش!');
+  }
 });
 
 client.login(process.env.DISCORD_TOKEN);
